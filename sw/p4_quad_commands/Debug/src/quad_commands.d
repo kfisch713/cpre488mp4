d9250fdc5292fb4e0fbe1b4ddb6e0ae59c1d4968
Fischer <kfisch13@iastate.edu>
2017-03-30 18:51:28 -0500

    bluetooth works. having problems arming

---
+++ sw/p4_quad_commands/Debug/src/quad_commands.d
@@ -0,0 +1,45 @@
+src/quad_commands.d: ../src/quad_commands.c \
+ ../../system_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../system_bsp/ps7_cortexa9_0/include/xuartps_hw.h ../src/platform.h \
+ ../src/platform_config.h
+
+../../system_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../system_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
---
+++ sw/p4_quad_commands/Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/platform.c \
+../src/quad_commands.c 
+
+LD_SRCS += \
+../src/lscript.ld 
+
+OBJS += \
+./src/platform.o \
+./src/quad_commands.o 
+
+C_DEPS += \
+./src/platform.d \
+./src/quad_commands.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM gcc compiler'
+	arm-xilinx-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -I../../system_bsp/ps7_cortexa9_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
